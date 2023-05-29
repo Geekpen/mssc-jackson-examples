b0324423998ae8315d7d94ec68e0a32099767ab8
Abungu Brian <brianabungu@gmail.com>
2023-05-29 22:25:08 +0300

    Setting Property Nams with Jackson

--- src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoTest.java
+++ src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoTest.java
@@ -27,7 +27,7 @@ class BeerDtoTest extends BaseTest{
 
     @Test
     void testDeserializeDto() throws IOException {
-        String json = "{\"id\":\"06411d4a-3112-4ea4-afee-96a37fef81c8\",\"beerName\":\"BeerName\",\"beerStyle\":\"Ale\",\"upc\":123123123123,\"price\":12.99,\"createdDate\":\"2023-05-29T21:55:39.394744+03:00\",\"lastUpdatedDate\":\"2023-05-29T21:55:39.394744+03:00\"}";
+        String json = "{\"beerId\":\"06411d4a-3112-4ea4-afee-96a37fef81c8\",\"beerName\":\"BeerName\",\"beerStyle\":\"Ale\",\"upc\":123123123123,\"price\":12.99,\"createdDate\":\"2023-05-29T21:55:39.394744+03:00\",\"lastUpdatedDate\":\"2023-05-29T21:55:39.394744+03:00\"}";
 
         BeerDto dto = objectMapper.readValue(json, BeerDto.class);
 
